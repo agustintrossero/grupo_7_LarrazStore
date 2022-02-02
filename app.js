ea2d7b26002fcd9c892680147a1f34b41dee3170
Juan Larraz <contactojuanlarraz@gmail.com>
2022-02-02 17:28:21 -0300

    Estuve trabajando en mi parte

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const app = express();
 const pathFile = path.resolve(__dirname, './public');
 app.use(express.static(pathFile));
 
-app.listen (5000);
+app.listen(5000, ()=>{
+    console.log('Servidor funcionando');
+});
 
-app.get('/home' , (req,res) => {res.sendfile(path.resolve(__dirname, './views/index.html'))});
+app.get('/' , (req,res) => {res.sendfile(path.resolve(__dirname, './views/index.html'))});
 app.get('/products' , (req,res) => {res.sendfile(path.resolve(__dirname, './views/product-detail.html'))});
 app.get('/cart' , (req,res) => {res.sendfile(path.resolve(__dirname, './views/product-cart.html'))});
 app.get('/register' , (req,res) => {res.sendfile(path.resolve(__dirname, './views/register.html'))});
