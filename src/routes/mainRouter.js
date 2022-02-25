8b4ce3100b8f58c6bdbed775f6da4baeb9b99920
agustintrossero <agustintrossero@gmail.com>
2022-02-25 16:22:30 +0100

    instale la dependecia EJS, ordene la estructura de carpetas, arme todas las rutas nuevas y deje comentadas las viejas, cambie los html por .ejs, cree los archivos mainRouter.js y main Controller.js dentro de sus respectivas carpetas, como tuve que meter el APP.js dentro del SRC junto con las VIEWS hice el cambio en package.json para que corra el proyecto con npm start     o    para que corra nodemon  npm run dev

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -1,19 +1,14 @@
-/*
-const controllador = {
-    index: (req, res) => {
-        return res.render("index")
-    },
-    login: (req, res) => {
-        return res.render("login")
-    },
-    cart: (req, res) => {
-        return res.render("cart")
-    },
-    product: (req, res) => {
-        return res.render("product")
-    },
-    register: (req, res) => {
-        return res.render("register")
-    },
-}
-*/
+
+const express = require("express");
+
+const router = express.Router();
+
+const controlador = require("../controllers/mainController");
+
+router.get("/", controlador.index)
+router.get("/login", controlador.login)
+router.get("/cart", controlador.cart)
+router.get("/products", controlador.products)
+router.get("/register", controlador.register)
+
+module.exports = router
