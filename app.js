a722ccd349e237c91becee48b3b645172b8d38cd
agustintrossero <agustintrossero@gmail.com>
2022-02-25 13:21:19 +0100

    instale ejs cree SRC con carpeta controllers y routes, estan comentados los cambios y comiteando para ver porq desaparecio todo el css del proyecto

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ const app = express();
 const pathFile = path.resolve(__dirname, './public');
 app.use(express.static(pathFile));
 
+/*
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname,'/views'));
+*/
+
 app.listen (5000), () => {
     console.log("Larraz Store en funcionamiento!")
 };
 
-app.get('/home' , (req,res) => {res.sendfile(path.resolve(__dirname, './views/index.html'))});
-app.get('/products' , (req,res) => {res.sendfile(path.resolve(__dirname, './views/product-detail.html'))});
-app.get('/cart' , (req,res) => {res.sendfile(path.resolve(__dirname, './views/product-cart.html'))});
-app.get('/register' , (req,res) => {res.sendfile(path.resolve(__dirname, './views/register.html'))});
-app.get('/login' , (req,res) => {res.sendfile(path.resolve(__dirname, './views/login.html'))});
+app.get('/' , (req,res) => {res.sendFile(path.resolve(__dirname, './views/index.html'))});
+app.get('/products' , (req,res) => {res.sendFile(path.resolve(__dirname, './views/product-detail.html'))});
+app.get('/cart' , (req,res) => {res.sendFile(path.resolve(__dirname, './views/product-cart.html'))});
+app.get('/register' , (req,res) => {res.sendFile(path.resolve(__dirname, './views/register.html'))});
+app.get('/login' , (req,res) => {res.sendFile(path.resolve(__dirname, './views/login.html'))});
 
---
+++ src/routes/mainRouter.js
@@ -0,0 +1,19 @@
+/*
+const controllador = {
+    index: (req, res) => {
+        return res.render("index")
+    },
+    login: (req, res) => {
+        return res.render("login")
+    },
+    cart: (req, res) => {
+        return res.render("cart")
+    },
+    product: (req, res) => {
+        return res.render("product")
+    },
+    register: (req, res) => {
+        return res.render("register")
+    },
+}
+*/
