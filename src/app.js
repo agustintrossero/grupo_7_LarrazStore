d6957a7864bc97923698a9a88514fde239281b3d
agustintrossero <agustintrossero@gmail.com>
2022-03-15 14:15:43 +0100

    APP.JS instale method override hice require de method override hice que la app use de la libreria express el .urlencoded y .json para capturar la info de los formularios ya que el proyecto no fue iniciado con express generator
    Cree dentro de la carpeta ROUTES
    userRouter.js
    productRouter.js
    
    cree en la carpeta de CONTROLLERS
    productController.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const mainRoutes = require("./routes/mainRouter")
+const methodOverride =  require('method-override'); // Pasar poder usar los métodos PUT y DELETE
 
 // const pathFile = path.resolve(__dirname, './public');
 //app.use(express.static(pathFile));
 app.use(express.static("public"))
-
+app.use(methodOverride('_method')); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
+app.use(express.urlencoded({ extended: false })); // Para capturar la info de los formularios
+app.use(express.json());
 
 app.set("view engine", "ejs");
-
 app.set('views',"./src/views");
 
+const mainRoutes = require("./routes/mainRouter")
 
 app.use("/", mainRoutes); 
 
--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -3,17 +3,17 @@ const express = require("express");
 
 const router = express.Router();
 
-const controlador = require("../controllers/mainController");
+const mainController = require("../controllers/mainController");
 
-router.get("/", controlador.index)
-router.get("/login", controlador.login)
-router.get("/cart", controlador.cart)
-router.get("/register", controlador.register)
-router.get("/products", controlador.products)
-router.get("/admin", controlador.admin)
-router.get("/products-agregar", controlador.agregar)
-router.get("/products-modificar", controlador.modificar)
-router.get("/products-eliminar", controlador.eliminar)
+router.get("/", mainController.index)
+router.get("/login", mainController.login)
+router.get("/cart", mainController.cart)
+router.get("/register", mainController.register)
+router.get("/products", mainController.products)
+router.get("/admin", mainController.admin)
+router.get("/products-agregar", mainController.agregar)
+router.get("/products-modificar", mainController.modificar)
+router.get("/products-eliminar", mainController.eliminar)
 
 
 module.exports = router
