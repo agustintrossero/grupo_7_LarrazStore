c74d9f173be15aef693a8eb6ec80e56097f2c79e
juanitolarraz <contactojuanlarraz@gmail.com>
2022-05-31 19:46:01 -0300

    chicos arranque con las validaciones en el front, pusheo parte de mi codigo para que no rompa con nada. Faltaria que puedan hacer andar el back del login para poder meterle las validaciones del front

---
+++ public/validaciones/js-front-login.js
@@ -0,0 +1,1 @@
+console.log('muchachos ya lo deje vinculado con el ejs de login, faltaria acomodar el back de login asi implemento estas magias en el front')
---
+++ public/validaciones/js-front-register.js
@@ -0,0 +1,52 @@
+window.addEventListener("load", function () {
+    let formulario = document.querySelector('form.user-form');
+    
+    formulario.addEventListener('submit', function(e){
+
+        let errores = [];
+
+
+        let campoNombre = document.querySelector('');
+
+        if (campoNombre.value == ""){
+            errores.push("El campo de nombre tiene que estar completo");
+        } else if (campoNombre.value.lenght < 2) {
+            errores.push("El campo de nombre debe tener al menos dos caracteres");
+        }
+
+        let campoApellido = document.querySelector('');
+
+        if (campoApellido.value == ""){
+            errores.push("El campo de apellido tiene que estar completo");
+        } else if (campoApellido.value.lenght < 2) {
+            errores.push("El campo de apellido debe tener al menos dos caracteres");
+        }
+
+        let campoUsername = document.querySelector('');
+
+        if (campoUsername.value == ""){
+            errores.push("El campo de usuario tiene que estar completo");
+        } else if (campoUsername.value.lenght < 2) {
+            errores.push("El campo de usuario debe tener al menos dos caracteres");
+        }
+
+        let campoEmail = document.querySelector('');
+
+        if (campoEmail.value == ""){
+            errores.push("El campo de email tiene que estar completo");
+        }
+
+        if (errores.length > 0) {
+            e.preventDefault()
+
+            let ulErrores = document.querySelector('div.errores ul')
+
+            for (let i = 0; i < errores.length; i++) {
+
+                ulErrores.innerHTML += "<li>" + errores[i] + "</li>"
+                
+            }
+        }
+
+    })
+})
