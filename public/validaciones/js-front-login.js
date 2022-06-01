ef1c5f963b632d624cbc3b3d3d0eda485cca6e6f
juanitolarraz <contactojuanlarraz@gmail.com>
2022-06-01 15:16:52 -0300

    falta pulir unas cositas en el front validator del register, el validator del login acaba de quedar 100% funcional

--- public/validaciones/js-front-login.js
+++ public/validaciones/js-front-login.js
@@ -2,7 +2,7 @@ const expresiones = {
 	usuario: /^[a-zA-Z0-9\_\-]{2,16}$/, 
 	nombre: /^[a-zA-ZÀ-ÿ\s]{2,40}$/, 
     apellido: /^[a-zA-ZÀ-ÿ\s]{2,40}$/, 
-	password: /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9]{8,16}$/, 
+	password: /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9]{8,16}$/, // tuvimos que sacar el tema de los caracteres especiales porque no habia caso de que nos lo tome.
 	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 }
 
@@ -17,33 +17,25 @@ window.addEventListener('load', (e) =>{
 
     email.addEventListener('keyup', (e) =>{
         if (expresiones.email.test(email.value)) {
-            errorDivEmail.innerHTML = '<p>tuki</p>'
             if (errorDivEmail.classList.contains('text-danger')) {
                 errorDivEmail.classList.remove('text-danger')
-                errorDivEmail.classList.add('text-success')
+                errorDivEmail.innerHTML = ''
             }
         } else {
-            errorDivEmail.innerHTML = '<p>Esto esta hecho para el orto</p>'
-            if (errorDivEmail.classList.contains('text-success')) {
-                errorDivEmail.classList.remove('text-success')
+            errorDivEmail.innerHTML = '<p>Debes escribir un formato de email válido.</p>'
                 errorDivEmail.classList.add('text-danger')
-            }
         }
     })
 
     password.addEventListener('keyup', (e) =>{
         if (expresiones.password.test(password.value)) {
-            errorDivPassword.innerHTML = '<p>tuki</p>'
             if (errorDivPassword.classList.contains('text-danger')) {
                 errorDivPassword.classList.remove('text-danger')
-                errorDivPassword.classList.add('text-success')
+                errorDivPassword.innerHTML = ''
             }
         } else {
-            errorDivPassword.innerHTML = '<p>Esto esta hecho para el orto</p>'
-            if (errorDivPassword.classList.contains('text-success')) {
-                errorDivPassword.classList.remove('text-success')
+            errorDivPassword.innerHTML = '<p>La contraseña debe incluir letras mayúsculas, minúsculas, como mínimo un número y un cáracter especial.</p>'
                 errorDivPassword.classList.add('text-danger')
-            }
         }
     })
 
